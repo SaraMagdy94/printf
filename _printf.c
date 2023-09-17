6329127f35219644835fff24e84e18ccc2e2da73
SaraMagdy94 <engazharya@gmail.com>
2023-09-17 22:35:18 +0300

    s

--- _printf.c
+++ _printf.c
@@ -1,60 +1,61 @@
-#include <unistd.h>
 #include <stdarg.h>
+#include <unistd.h>
 #include "main.h"
+#include <stdio.h>
 /**
  * _printf - printf function
  * @format: Format string
  * @...: Variable arguments
+ * _strlen - Calculate the length of a string.
+ * @str: The input string.
  * Return: Number of characters printed
  */
+size_t _strlen(const char *str)
+{
+size_t l = 0;
+while (str[l])
+l++;
+return (l);
+}
 int _printf(const char *format, ...)
 {
 int as = 0;
 va_list g;
-
 va_start(g, format);
 
 while (*format)
 {
-if (*format == '%' && (*(format + 1) == 'c' || *(format + 1) == 's'))
+if (*format == '%' && *(format + 1))
 {
-if (*(format + 1) == 'c')
+format++;
+switch (*format)
 {
-char x = va_arg(g, int);
-write(1, &x, 1);
-as++;
+case 'c':
+{
+char c = va_arg(g, int);
+as = as + write(1, &c, 1);
+break;
+}
+case 's':
+{
+const char *str = va_arg(g, const char *);
+as = as + write(1, str, _strlen(str));
+break;
+}
+case '%':
+as = as + write(1, "%", 1);
+break;
+default:
+as = as + write(1, "%", 1);
+as = as + write(1, format, 1);
+}
 }
 else
-{
-char *sr = va_arg(g, char *);
-if (sr)
-{
-while (*sr)
-{
-write(1, sr++, 1);
-as++;
-}
-}
-else
-{
-write(1, "(null)", 6);
-as = as + 6;
-}
-}
-format = format + 2;
-}
-else if (*format == '%' && (*(format + 1) == '%' || *(format + 1) == '\0'))
-{
-write(1, "%", 1);
-as++;
-format = format + 2;
-}
-else
-{
-write(1, format++, 1);
-as++;
-}
+as = as + write(1, format, 1);
+format++;
 }
 va_end(g);
 return (as);
 }
+
+
