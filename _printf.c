118d9f6487ca32a0f9bf45ed5e69d794a69d0831
SaraMagdy94 <engazharya@gmail.com>
2023-09-18 03:40:23 +0300

    s

--- _printf.c
+++ _printf.c
@@ -54,6 +54,25 @@ as = as + 6;
 }
 format = format + 2;
 }
+else if (*format == '%' && (*(format + 1) == 'u' || *(format + 1) == 'o' || *(format + 1) == 'x' || *(format + 1) == 'X'))
+{
+unsigned int r = va_arg(g, unsigned int);
+char buffer[32];
+int i = 0;
+
+if (*(format + 1) == 'u')
+i = sprintf(buffer, "%u", r);
+else if (*(format + 1) == 'o')
+i = sprintf(buffer, "%o", r);
+else if (*(format + 1) == 'x')
+i = sprintf(buffer, "%x", r);
+else if (*(format + 1) == 'X')
+i = sprintf(buffer, "%X", r);
+
+write(1, buffer, i);
+as =  as + i;
+format = format + 2;
+}
 else if (*format == '%' && (*(format + 1) == '%' || *(format + 1) == '\0'))
 {
 write(1, "%", 1);
