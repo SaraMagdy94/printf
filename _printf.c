cd4d958b921b3d329a5846e5ccb194a0927932df
SaraMagdy94 <engazharya@gmail.com>
2023-09-18 17:48:30 -0700

    s

--- _printf.c
+++ _printf.c
@@ -19,13 +19,21 @@ va_start(g, format);
 
 while (*format)
 {
-if (*format == '%' && (*(format + 1) == 'c' || *(format + 1) == 's'))
+if (*format == '%' && (*(format + 1) == 'd' || *(format + 1) == 'i'))
+{
+int r = va_arg(g, int);
+char buffer[12];
+int i = sprintf(buffer, "%d", r);
+write(1, buffer, i);
+as = as + i;
+format = format + 2;
+}
+else if (*format == '%' && (*(format + 1) == 'c' || *(format + 1) == 's'))
 {
 if (*(format + 1) == 'c')
 {
 char x = va_arg(g, int);
-buffer[0] = x;
-write(1, buffer, 1);
+write(1, &x, 1);
 as++;
 }
 else
@@ -47,18 +55,6 @@ as = as + 6;
 }
 format = format + 2;
 }
-<<<<<<< HEAD
-else if (*format == '%' && (*(format + 1) == 'd' || *(format + 1) == 'i'))
-{
-int r = va_arg(g, int);
-char buffer[12];
-int i = sprintf(buffer, "%d", r);
-write(1, buffer, i);
-as = as + i;
-format = format + 2;
-}
-=======
->>>>>>> eee0e2f5625479c46bc053aa4b981817e0f82c66
 else if (*format == '%' && (*(format + 1) == 'u' ||
 *(format + 1) == 'o' || *(format + 1) == 'x' || *(format + 1) == 'X'))
 {
@@ -79,19 +75,31 @@ write(1, buffer, i);
 as =  as + i;
 format = format + 2;
 }
+else if (*format == '%' && *(format + 1) == 'p')
+{
+	void *p = va_arg(g, void *);
+	int l;
+
+	sprintf(buffer, "%p", p);
+	l = strlen(buffer);
+	write(1, buffer, l);
+	as = as + l;
+	format = format + 2;
+}
 else if (*format == '%' && (*(format + 1) == '%' || *(format + 1) == '\0'))
 {
-buffer[0] = '%';
-write(1, buffer, 1);
+write(1, "%", 1);
 as++;
 format = format + 2;
 }
 else
 {
-buffer[0] = *format++;
-write(1, buffer, 1);as++;
+write(1, format++, 1);
+as++;
 }
 }
 va_end(g);
 return (as);
 }
+
+
