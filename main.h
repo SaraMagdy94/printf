0131e2d289889efd2910deb866c959e72e201e07
Rawy450 <Rawy450>
2023-09-18 17:19:26 -0700

    my first commit

--- main.h
+++ main.h
@@ -3,15 +3,4 @@
 
 int _printf(const char *format, ...);
 
-void printChar(char c, int *as);
-
-void printString(const char *str, int *as);
-
-void printInteger(int num, int *as);
-
-void printUnsigned(unsigned int num, int *as);
-
-void printPointer(void *ptr, int *as);
-
 #endif /* MAIN_H */
-
